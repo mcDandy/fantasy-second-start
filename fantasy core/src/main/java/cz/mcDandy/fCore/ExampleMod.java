95a08e26b8f8851115b013a36d9da4b37a7a80f1
mcDandy <danda.martinek@gmail.com>
2017-02-11 16:01:14 +0100

    added proxis

--- fantasy core/src/main/java/cz/mcDandy/fCore/ExampleMod.java
+++ fantasy core/src/main/java/cz/mcDandy/fCore/ExampleMod.java
@@ -3,14 +3,17 @@ package cz.mcDandy.fCore;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+    public static final String MODID = "fCore";
+    public static final String VERSION = "0.0.1";
+    @SidedProxy(clientSide="cz.mcDandy.fCore.ClientProxy", serverSide="cz.mcDandy.fCore.ServerProxy")
+    public static CommonProxy proxy;  
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
